a5341ce9f86ae334c5ef7fe0047cdb607a23ac80
paperfront <mattlacayo@berkeley.edu>
2020-10-10 15:09:10 -0700

    Updated driver to take command line arguments

--- driver.py
+++ driver.py
@@ -1,4 +1,10 @@
 from face_write import write_all_faces
+import sys
 
 if __name__ == "__main__":
-    write_all_faces('avengers.jpg', 'samy_2.png')
+    argv = sys.argv
+    if len(argv) != 3:
+        print("Proper usage: driver.py [background_path] [source path]")
+        return
+    background_path, source_path = argv[1:]
+    write_all_faces(background_path, source_path)
